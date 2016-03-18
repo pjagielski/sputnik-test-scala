161ae49d2a2877bcce31dc2a8bb5f02f5273e00b
Piotr Jagielski <pjg@touk.pl>
2016-03-18 11:25:22 +0100

    bad commit

---
+++ src/main/scala/FooBar.scala
@@ -0,0 +1,26 @@
+object FooBar {
+  def main(args: Array[String]) {
+    println("Test of sputnik with scalastyle")
+  }
+
+  private def returnUnsafe: Int = {
+    if (true) {
+      return 20
+    } else {
+      return 42
+    }
+  }
+
+  private def optionGetBadPractise: Int = {
+    val o = Option(42)
+    if (o.isDefined) {
+      o.get
+    } else {
+      50
+    }
+  }
+
+  def magicNumberBadPractise: Unit = {
+    var foo = 4345
+  }
+}
